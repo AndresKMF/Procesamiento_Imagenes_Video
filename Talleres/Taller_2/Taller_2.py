6efcb992a7f60fbd855416e641c565248f7b7e1f
AndresKMF <88415902+AndresKMF@users.noreply.github.com>
2021-08-24 19:10:46 -0500

    Add files via upload

---
+++ Talleres/Taller_2/Taller_2.py
@@ -0,0 +1,95 @@
+# Pontificia Universidad Javeriana
+# Procesamiento de Imágenes y Video
+# Luis Miguel Muñoz Flórez
+# Taller 2
+# main: Taller_2.py
+
+# Importar librerías
+import cv2
+import os
+from thetaFilter import *
+
+# Definición de funciones
+# promedio_imagen: función que promedia 4 imágenes
+def promedio_imagen(imagen_1, imagen_2, imagen_3, imagen_4):
+    imagen_promedio = (imagen_1 + imagen_2 + imagen_3 + imagen_4) / 4
+    return imagen_promedio
+
+# filtros_direccionales: función que aplica 4 distintos filtros a una imagen de entrada
+def filtros_direccionales( imagen_gris, theta_1, theta_2, theta_3, theta_4, delta_theta ):
+    # Filtro 1
+    filtro_1 = thetaFilter(imagen_gris)
+    filtro_1.set_theta(theta_1, delta_theta)
+    filtro_1.filtering()
+    str1 = "Imagen con F1: " + str(theta_1) + " grados"
+    cv2.imshow(str1, filtro_1.imagen_filtrada)
+    # cv2.imshow("Frecuencia F1", 255 * filtro_1.mascara)
+    # cv2.waitKey(0)
+
+    # Filtro 2
+    filtro_2 = thetaFilter(imagen_gris)
+    filtro_2.set_theta(theta_2, delta_theta)
+    filtro_2.filtering()
+    str2 = "Imagen con F2: " + str(theta_2) + " grados"
+    cv2.imshow(str2, filtro_2.imagen_filtrada)
+    # cv2.imshow("Frecuencia F2", 255 * filtro_2.mascara)
+    # cv2.waitKey(0)
+
+    # Filtro 3
+    filtro_3 = thetaFilter(imagen_gris)
+    filtro_3.set_theta(theta_3, delta_theta)
+    filtro_3.filtering()
+    str3 = "Imagen con F3: " + str(theta_3) + " grados"
+    cv2.imshow(str3, filtro_3.imagen_filtrada)
+    # cv2.imshow("Frecuencia F3", 255 * filtro_3.mascara)
+    # cv2.waitKey(0)
+
+    # Filtro 4
+    filtro_4 = thetaFilter(imagen_gris)
+    filtro_4.set_theta(theta_4, delta_theta)
+    filtro_4.filtering()
+    str4 = "Imagen con F4: " + str(theta_4) + " grados"
+    cv2.imshow(str4, filtro_4.imagen_filtrada)
+    # cv2.imshow("Frecuencia F4", 255 * filtro_4.mascara)
+    # cv2.waitKey(0)
+
+    # Promedio
+    imagen_promedio = promedio_imagen(filtro_1.imagen_filtrada, filtro_2.imagen_filtrada, filtro_3.imagen_filtrada, filtro_4.imagen_filtrada)
+    cv2.imshow("Imagen Promedio", imagen_promedio)
+    cv2.waitKey(0)
+
+# Main
+if __name__ == '__main__':
+    ruta = "C:\Imagenes_Video\Images\huellas"
+    nombre_imagen = "01_4.tif"
+    ruta_archivo = os.path.join(ruta, nombre_imagen)
+    # Leer imagen
+    imagen = cv2.imread(ruta_archivo)
+    # Convertir imagen a grises
+    imagen_gris = cv2.cvtColor(imagen, cv2.COLOR_BGR2GRAY)
+
+    # Imagen original
+    cv2.imshow("Imagen original", imagen)
+
+    # Aplicar filtros dirreccionales
+    filtros_direccionales(imagen_gris, 0, 45, 90, 135, 5)
+
+    ### PRUEBAS
+    '''
+    # Crear Objeto theta_filter de la Clase thetaFilter
+    theta_filter = thetaFilter(imagen_gris)
+    # Asignar ángulo y umbral
+    theta_filter.set_theta(0, 5)
+    # Filtrado
+    theta_filter.filtering()
+
+    # Imagen original
+    cv2.imshow("Imagen original", imagen)
+    # FFT imagen
+    cv2.imshow("Transformada de Fourier de la Imagen", theta_filter.fft_imagen)
+    # Respuesta en frecuencia del filtro
+    cv2.imshow("Respuesta en frecuencia del filtro", 255 * theta_filter.mascara)
+    # Imagen original filtrada
+    cv2.imshow("Imagen filtrada", theta_filter.imagen_filtrada)
+    cv2.waitKey(0)
+    '''
---
+++ Talleres/Taller_2/thetaFilter.py
@@ -0,0 +1,90 @@
+# Pontificia Universidad Javeriana
+# Procesamiento de Imágenes y Video
+# Luis Miguel Muñoz Flórez
+# Taller 2
+# Clase thetaFilter
+
+# Importar librerías
+import cv2
+import numpy as np
+
+# Construcción de la clase thetaFilter
+class thetaFilter:
+
+    # Atributos de la clase
+    imagen_gris = []
+    fft_imagen = []
+    mascara = []
+    imagen_filtrada = []
+    theta = 0
+    delta_theta = 0
+
+    # Constructor de la clase
+    def __init__(self, imagen_gris):
+        self.imagen_gris = imagen_gris
+
+    # Método set_theta: recibir los parámetros theta y delta_theta que definirán
+    # la respuesta del filtro
+    def set_theta(self, theta, delta_theta):
+        self.theta = theta
+        self.delta_theta = delta_theta
+
+    # Método filtering: permite solo el paso de componentes de frecuencia orientados en theta - delta_theta
+    # y en theta + delta_theta
+    def filtering(self):
+        # Transformada de fourier (FFT) de la imagen en gris
+        fft_imagen_gris = np.fft.fft2(self.imagen_gris)
+        # Desplazar la FFT de la imagen
+        fft_imagen_gris_desplazada = np.fft.fftshift(fft_imagen_gris)
+
+        # Visualización de la FFT
+        # Valor absoluto de la FFT desplazada
+        fft_imagen_gris_magnitud = np.absolute(fft_imagen_gris_desplazada)
+        # Aplicamos algortimo para poder visualizar
+        fft_imagen_vista = np.log(fft_imagen_gris_magnitud + 1)
+        # Normalizar la FFT
+        fft_imagen_vista = fft_imagen_vista / np.max(fft_imagen_vista)
+        # Mostrar FFT de la imagen
+        self.fft_imagen = fft_imagen_vista
+        # cv2.imshow("FFT Imagen", fft_imagen_vista)
+
+        # Pre procesamiento
+        # Extracción del tamaño de la imagen
+        num_filas, num_columnas = (self.imagen_gris.shape[0], self.imagen_gris.shape[1])
+        # Vectores de filas y columnas
+        enum_filas = np.linspace(0, num_filas - 1, num_filas)
+        enum_columnas = np.linspace(0, num_columnas - 1, num_columnas)
+        # Matriz para iterar en toda la imagen
+        iterador_columnas, iterador_filas = np.meshgrid(enum_columnas, enum_filas)
+        # Encontrar el centro de la imagen
+        mitad_filas = num_filas / 2 - 1 # en filas
+        mitad_columnas = num_columnas / 2 - 1 # en columnas
+
+        # Máscara del filtro
+        mascara_1 = np.zeros_like(self.imagen_gris)
+        # Límite inferior
+        idx_bajo = (180 * np.arctan2((iterador_columnas - mitad_columnas), (iterador_filas - mitad_filas)) / np.pi ) > (self.theta - self.delta_theta)
+        # Límite superior
+        idx_alto = (180 * np.arctan2((iterador_columnas - mitad_columnas), (iterador_filas - mitad_filas)) / np.pi ) < (self.theta + self.delta_theta)
+        # Realizar AND entre los límites
+        idx_total = np.bitwise_and(idx_bajo, idx_alto)
+        # Traer solamente las componentes en frecuenciaque cumplan el rango
+        mascara_1[idx_total] = 1
+        # cv2.imshow("Mascara", mascara)
+        # Rotar 180 grados la primera máscara
+        mascara_2 = cv2.rotate(mascara_1, cv2.ROTATE_180)
+        # Juntar las dos máscaras
+        mascara = np.bitwise_or(mascara_1, mascara_2)
+        # Filtrado
+        # Aplicar máscara a la FFT de la imagen desplazada
+        fft_filtrada = fft_imagen_gris_desplazada * mascara
+        # Guardar máscara
+
+        self.mascara = mascara
+        # FFT inversa para obtener la imagen filtrada
+        imagen_filtrada = np.fft.ifft2(np.fft.fftshift(fft_filtrada))
+        imagen_filtrada = np.absolute(imagen_filtrada)
+        imagen_filtrada = imagen_filtrada / np.max(imagen_filtrada)
+        # Guardar imagen filtrada
+        self.imagen_filtrada = imagen_filtrada
+
